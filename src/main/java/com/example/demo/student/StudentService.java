95f1b210bb85eb65a3acd746bcb5d486d29b7b1e
Tiger <billath131908@gmail.com>
2024-10-24 17:27:15 +0300

    Add IStudentRepository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final IStudentRepository _studentRepository;
+
+    @Autowired
+    public StudentService(IStudentRepository studentRepository) {
+        _studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Vasilis",
-                        "vas@gmail.com",
-                        LocalDate.of(2001, Month.MARCH, 19),
-                        23
-                ));
+        return _studentRepository.findAll();
     }
 }
