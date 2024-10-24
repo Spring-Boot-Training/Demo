2197b49570c63b866a848cf856508cff97c015db
Tiger <billath131908@gmail.com>
2024-10-24 17:36:22 +0300

    Insert rows in DB

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            IStudentRepository repository) {
+        return args -> {
+            Student vas = new Student(
+                    1L,
+                    "Vasilis",
+                    "vas@gmail.com",
+                    LocalDate.of(2001, Month.MARCH, 19),
+                    23
+            );
+
+            Student niko = new Student(
+                    "Nikola",
+                    "nik@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 4),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(vas, niko)
+            );
+        };
+    }
+}
